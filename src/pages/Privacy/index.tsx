cd485fe511b581434b6b9557bbdf8d106828372a
C1333183 Kleiser Nairobi De Oliveira <kleiser.nairobi@gmail.com>
2025-07-18 11:10:44 -0300

    correções termo e privacidade

--- src/pages/Privacy/index.tsx
+++ src/pages/Privacy/index.tsx
@@ -15,35 +15,61 @@ export function Privacy() {
         <Header title="Política de privacidade" onPress={() => navigation.goBack()} />
         <Text style={styles.subtitle}>
           Esta Política de Privacidade descreve como o aplicativo Saber Espírita coleta, utiliza e
-          protege as informações pessoais dos usuários. Ao utilizar o aplicativo, você concorda com
-          os termos desta política. Se você não concordar, por favor, não continue a usar o
-          aplicativo.
+          protege as informações dos usuários. Ao utilizar o aplicativo, você concorda com os termos
+          desta política. Se você não concordar, por favor, não continue a usar o aplicativo.
         </Text>
         <ScrollView showsVerticalScrollIndicator={false} style={{ marginTop: 20 }}>
           <Text style={styles.item}>1. Coleta de informações</Text>
           <View style={styles.containerItem}>
             <Text style={styles.description}>
-              O aplicativo Saber Espírita” não coleta nenhum dado pessoal identificável dos
-              usuários. Embora haja uma tela de identificação onde você pode inserir um nome, saiba
-              que essa informação é estritamente opcional e não será utilizada para rastrear,
-              identificar ou armazenar informações sobre você.
+              Ao se cadastrar no aplicativo Saber Espírita, coletamos as seguintes informações:
+            </Text>
+            <Text style={styles.description}>
+              - Apelido/Nome (obrigatório): Usado para sua identificação dentro do aplicativo,
+              placares e para personalizar sua experiência.
+            </Text>
+            <Text style={styles.description}>
+              - Endereço de e-mail (obrigatório): Usado para login, recuperação de senha e,
+              opcionalmente, para comunicações importantes do aplicativo, como notícias,
+              atualizações ou informações sobre sua conta.
+            </Text>
+            <Text style={styles.description}>
+              - Senha (obrigatória): Armazenada de forma criptografada para proteger o acesso à sua
+              conta.
             </Text>
           </View>
           <Text style={styles.item}>2. Uso das Informações</Text>
           <View style={styles.containerItem}>
             <Text style={styles.description}>
-              O aplicativo "Saber Espírita" não utiliza as informações inseridas na tela de
-              identificação para nenhum propósito. O nome fornecido pelo usuário não será associado
-              a nenhuma atividade ou ação dentro do aplicativo. Ele serve apenas como uma forma de
-              personalizar a experiência do usuário durante a utilização do quiz.
+              As informações coletadas são utilizadas exclusivamente para os seguintes propósitos:
+            </Text>
+
+            <Text style={styles.description}>
+              - Gerenciamento da Conta: Para permitir que você faça login, acesse seu perfil e
+              recupere sua senha.
+            </Text>
+            <Text style={styles.description}>
+              - Personalização da Experiência: O apelido é usado para personalizar a interface do
+              quiz e exibido em placares de pontuação.
+            </Text>
+            <Text style={styles.description}>
+              - Comunicações: Seu e-mail poderá ser usado para enviar informações relacionadas ao
+              aplicativo (como avisos de serviço, novidades ou recuperação de conta), caso você não
+              desative essa opção. O nome, se fornecido, pode ser usado para personalizar essas
+              comunicações.
+            </Text>
+            <Text style={styles.description}>
+              - Melhoria do Serviço: As informações agregadas e anônimas (não identificáveis
+              pessoalmente) sobre o uso geral do aplicativo podem ser analisadas para entender
+              padrões de uso e melhorar a experiência do usuário.
             </Text>
           </View>
           <Text style={styles.item}>3. Compartilhamento de Informações</Text>
           <View style={styles.containerItem}>
             <Text style={styles.description}>
-              O aplicativo "Saber Espírita" não compartilha nenhuma informação pessoal ou qualquer
-              outro dado inserido pelos usuários com terceiros, incluindo empresas, organizações ou
-              indivíduos.
+              O aplicativo Saber Espírita não compartilha suas informações pessoais (apelido,
+              e-mail, senha, nome) com terceiros, incluindo empresas, organizações ou indivíduos,
+              exceto quando exigido por lei.
             </Text>
           </View>
           <Text style={styles.item}>4. Cookies e Tecnologias Similares</Text>
@@ -56,16 +82,26 @@ export function Privacy() {
           <Text style={styles.item}>5. Segurança</Text>
           <View style={styles.containerItem}>
             <Text style={styles.description}>
-              A segurança das informações dos usuários é importante para nós. Embora o aplicativo
-              "Saber Espírita" não colete informações pessoais, tomamos medidas razoáveis para
-              proteger qualquer dado fornecido pelos usuários no aplicativo.
+              A segurança das suas informações é muito importante para nós. Embora nenhuma
+              transmissão de dados pela internet seja 100% segura, tomamos medidas razoáveis para
+              proteger as informações que coletamos, como a criptografia da sua senha.
             </Text>
           </View>
           <Text style={styles.item}>6. Menores de Idade</Text>
           <View style={styles.containerItem}>
             <Text style={styles.description}>
-              O aplicativo "Saber Espírita" é destinado a todas as idades e não coleta nenhuma
-              informação pessoal identificável, tornando-o seguro para uso por menores de idade.
+              O aplicativo Saber Espírita aborda a doutrina espírita e é destinado principalmente a
+              adolescentes (a partir de 13 anos) e adultos. Embora não contenha material impróprio
+              para outras idades, seu conteúdo pode ser mais bem compreendido por um público mais
+              maduro.
+            </Text>
+            <Text style={styles.description}>
+              - Caso você seja menor de 18 anos, certifique-se de ter a permissão de seus pais ou
+              responsáveis antes de se cadastrar, usar o aplicativo e fornecer qualquer informação
+              pessoal, incluindo o e-mail e o nome. Não coletamos intencionalmente informações de
+              menores de 13 anos. Se tomarmos conhecimento de que coletamos informações de um menor
+              de 13 anos sem o consentimento dos pais, tomaremos medidas para remover essas
+              informações de nossos servidores.
             </Text>
           </View>
           <Text style={styles.item}>7. Alterações na Política de Privacidade</Text>
@@ -73,20 +109,19 @@ export function Privacy() {
             <Text style={styles.description}>
               Reservamo-nos o direito de atualizar ou modificar esta Política de Privacidade a
               qualquer momento. Quaisquer alterações significativas serão notificadas através de uma
-              atualização no aplicativo ou em nosso site.
+              atualização no aplicativo ou em nosso site, e a data da "Última Atualização" será
+              revisada.
             </Text>
           </View>
           <Text style={styles.item}>8. Contato</Text>
           <View style={styles.containerItem}>
             <Text style={styles.description}>
               Se você tiver alguma dúvida ou preocupação relacionada a esta Política de Privacidade
-              ou ao uso do aplicativo "Saber Espírita", por favor, entre em contato conosco pelo
+              ou ao uso do aplicativo Saber Espírita, por favor, entre em contato conosco pelo
               seguinte endereço de e-mail: quiz.saberespirita@gmail.com
             </Text>
           </View>
-          <Text style={styles.update}>
-            Esta Política de Privacidade foi atualizada em 01/04/2025.
-          </Text>
+          <Text style={styles.update}>Última Atualização: 01/07/2025.</Text>
         </ScrollView>
       </SafeAreaView>
     </GradientContainer>
--- src/pages/Terms/index.tsx
+++ src/pages/Terms/index.tsx
@@ -23,20 +23,29 @@ export function Terms() {
             <View style={styles.row}>
               <Text style={styles.subitem}>1.1</Text>
               <Text style={styles.description}>
-                O aplicativo Saber Espírita” destina-se a fins recreativos e educativos, oferecendo
-                um quiz com temática espírita. Não nos responsabilizamos por qualquer uso indevido
-                ou interpretação incorreta das informações contidas no aplicativo.
+                O aplicativo "Saber Espírita" tem como propósito promover o aprendizado e a
+                divulgação da doutrina espírita, por meio de um quiz interativo que incentiva a
+                autoavaliação, o estudo e o entretenimento. Seu uso é destinado a fins educativos e
+                recreativos.
               </Text>
             </View>
             <View style={styles.row}>
               <Text style={styles.subitem}>1.2</Text>
               <Text style={styles.description}>
-                Você concorda em utilizar o aplicativo de acordo com todas as leis e regulamentos
-                locais, nacionais e internacionais aplicáveis.
+                Espera-se que os usuários utilizem o aplicativo de forma ética e responsável,
+                respeitando os princípios da doutrina espírita e os termos aqui descritos. O uso
+                inadequado, ofensivo ou ilegal não é permitido.
+              </Text>
+            </View>
+            <View style={styles.row}>
+              <Text style={styles.subitem}>1.3</Text>
+              <Text style={styles.description}>
+                Embora o conteúdo tenha sido elaborado com cuidado e dedicação, o aplicativo não se
+                responsabiliza por eventuais interpretações incorretas ou aplicações individuais das
+                informações fornecidas.
               </Text>
             </View>
           </View>
-
           <Text style={styles.item}>2. Propriedade intelectual</Text>
           <View style={styles.containerItem}>
             <View style={styles.row}>
@@ -52,33 +61,30 @@ export function Terms() {
               <Text style={styles.description}>
                 Você não possui o direito de reproduzir, modificar, distribuir ou criar obras
                 derivadas com base no aplicativo "Saber Espírita" sem o consentimento expresso por
-                escrito de Kleiser Nairobi de Oliveira.
+                escrito do autor.
               </Text>
             </View>
           </View>
-
           <Text style={styles.item}>3. Conteúdo do usuário</Text>
           <View style={styles.containerItem}>
             <View style={styles.row}>
               <Text style={styles.subitem}>3.1</Text>
               <Text style={styles.description}>
-                O aplicativo "Saber Espírita" pode permitir que você insira um nome na tela de
-                identificação para personalizar a experiência do usuário. No entanto, você concorda
-                em não inserir nomes ou qualquer conteúdo que seja ofensivo, difamatório, ilegal ou
-                que viole os direitos de terceiros.
+                O aplicativo pode permitir que você insira um nome ou apelido na tela de
+                identificação para personalizar sua experiência. No entanto, você concorda em não
+                inserir nomes ou conteúdo ofensivo, difamatório, ilegal ou que viole os direitos de
+                terceiros.
               </Text>
             </View>
             <View style={styles.row}>
               <Text style={styles.subitem}>3.2</Text>
               <Text style={styles.description}>
-                Você mantém a propriedade do conteúdo que você insere no aplicativo, mas concede a
+                Você mantém a propriedade do conteúdo que insere no aplicativo, mas concede a
                 Kleiser Nairobi de Oliveira uma licença não exclusiva, global, irrevogável e
-                sublicenciável para usar, reproduzir, modificar e exibir esse conteúdo no
-                aplicativo.
+                sublicenciável para usá-lo, reproduzi-lo, modificá-lo e exibi-lo no aplicativo.
               </Text>
             </View>
           </View>
-
           <Text style={styles.item}>4. Limitação de responsabilidade</Text>
           <View style={styles.containerItem}>
             <View style={styles.row}>
@@ -93,13 +99,40 @@ export function Terms() {
               <Text style={styles.subitem}>4.2</Text>
               <Text style={styles.description}>
                 Kleiser Nairobi de Oliveira não será responsável por quaisquer danos diretos,
-                indiretos, incidentais, especiais ou consequenciais decorrentes do uso ou
-                impossibilidade de uso do aplicativo "Saber Espírita".
+                indiretos, incidentais, especiais ou consequenciais decorrentes do uso ou da
+                impossibilidade de uso do aplicativo.
               </Text>
             </View>
           </View>
+          <Text style={styles.item}>5. Idade Mínima e Consentimento Parental</Text>
+          <View style={styles.containerItem2}>
+            <Text style={styles.description}>
+              O uso do aplicativo está autorizado apenas para pessoas com idade igual ou superior a
+              13 anos. Se você for menor de 18 anos, é necessário obter o consentimento de seus pais
+              ou responsáveis legais para utilizar o aplicativo.
+            </Text>
+          </View>
 
-          <Text style={styles.item}>5. Alterações nos termos de uso</Text>
+          <Text style={styles.item}>6. Conformidade com Leis de Proteção de Dados</Text>
+          <View style={styles.containerItem2}>
+            <Text style={styles.description}>
+              Ao utilizar o aplicativo, você declara estar ciente de que seus dados serão tratados
+              de acordo com as legislações de proteção de dados aplicáveis, incluindo a Lei Geral de
+              Proteção de Dados (LGPD) no Brasil, o Regulamento Geral sobre a Proteção de Dados
+              (GDPR) na União Europeia e a Children's Online Privacy Protection Act (COPPA), nos
+              Estados Unidos, quando aplicável.
+            </Text>
+          </View>
+          <Text style={styles.item}>7. Serviços de Terceiros</Text>
+          <View style={styles.containerItem2}>
+            <Text style={styles.description}>
+              O aplicativo pode integrar serviços de terceiros para funcionalidades como
+              autenticação, armazenamento, análise de dados ou notificações. Esses serviços possuem
+              suas próprias políticas de privacidade e termos de uso, e podem coletar informações
+              conforme suas diretrizes.
+            </Text>
+          </View>
+          <Text style={styles.item}>8. Alterações nos termos de uso</Text>
           <View style={styles.containerItem2}>
             <Text style={styles.description}>
               Reservamo-nos o direito de modificar ou atualizar estes Termos de Uso a qualquer
@@ -107,17 +140,15 @@ export function Terms() {
               ou em nosso site.
             </Text>
           </View>
-
-          <Text style={styles.item}>6. Contato</Text>
+          <Text style={styles.item}>9. Contato</Text>
           <View style={styles.containerItem2}>
             <Text style={styles.description}>
               Se você tiver alguma dúvida ou preocupação relacionada a estes Termos de Uso ou ao uso
-              do aplicativo "Saber Espírita", por favor, entre em contato conosco pelo seguinte
-              endereço de e-mail: quiz.saberespirita@gmail.com
+              do aplicativo "Saber Espírita", por favor, entre em contato conosco pelo e-mail:
+              quiz.saberespirita@gmail.com
             </Text>
           </View>
-
-          <Text style={styles.update}>Este Termo de Uso foi atualizado em 01/04/2025.</Text>
+          <Text style={styles.update}>Última atualização: 01/07/2025</Text>
         </ScrollView>
       </SafeAreaView>
     </GradientContainer>
